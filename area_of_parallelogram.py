2323d8261f105dfacd8f0812cef3e197afa158ae
Fatima Azeem <109719817+fatima-azeem@users.noreply.github.com>
2024-06-06 19:19:17 +0500

    Program to find the Area of a Parallelogram

---
+++ area_of_parallelogram.py
@@ -0,0 +1,6 @@
+h = int(input("Enter the height of the parallelogram: "))
+b = int(input("Enter the base of the parallelogram: "))
+
+area = h * b
+
+print(f"The area of the parallelogram is {area}")
